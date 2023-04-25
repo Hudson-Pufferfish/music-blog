1f1a219bc0e08711446c6a7cefae792bba4e710b
Hudson Nguyen <hungson.nguyen811@email.com>
2023-04-24 20:13:35 -0500

    setup routing and basic layout + set up to connect to supabase client

---
+++ src/AllPosts.tsx
@@ -0,0 +1,9 @@
+import { Link, useParams } from "react-router-dom";
+
+const AllPosts = () => {
+  const { pageNumber } = useParams();
+
+  return <h2>All Posts; page: {pageNumber}</h2>;
+};
+
+export default AllPosts;
